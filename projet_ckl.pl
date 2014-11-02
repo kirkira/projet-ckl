4f6224e12db6cdbb662ec101b80c4d46c5475d9b
lorie-den-os <lorie.den.os@gmail.com>
2014-11-02 18:40:39 +0100

    Update export .TXT et .CSV (optionnel)
    Export TXT par defaut, export CSV si demandé : 3 fichiers separés.

--- projet_ckl.pl
+++ projet_ckl.pl
@@ -9,12 +9,16 @@ use Text::Language::Guess;
 use LWP::Protocol::https;
 use Lingua::Stem;
 use Text::Ngrams;
+use Class::CSV;
 
 my $query = "perl modules"; # requete par defaut
 my $guess = 0;
 my $stem = 0;
-GetOptions ('query=s' => \$query, 'guess' => \$guess, 'stem' => \$stem)
-or die("Erreur dans les arguments (syntaxe d'une requete : --query=\"la requete\", pour determiner la langue : --guess, pour utiliser le stemmer : --stem)\n");
+my $export_file = "ckl_results.txt";
+my $csvfile = 0;
+
+GetOptions ('query=s' => \$query, 'guess' => \$guess, 'stem' => \$stem, 'csvfile' => \$csvfile) 
+or die("Erreur dans les arguments (syntaxe d'une requete : --query=\"la requete\", pour determiner la langue : --guess, pour utiliser le stemmer : --stem, pour faire un export csv : --csvfile)\n");
 
 my $agent = LWP::UserAgent->new();
 my $extractor = HTML::ContentExtractor->new();
@@ -46,14 +50,14 @@ while ((my $result = $search->next) && ($count < 50)) {
             
 			if($stem != 0) {
 				my @text_array = split(/\s/,$text);
-				my $locale = 'en';
+				my $locale = 'en'; #langue par defaut
 				if($lang) {
 					$locale = $lang;
 				}
 				
 				$stemmer->set_locale($locale);
-	            my @stemmed_words = @{$stemmer->stem(@text_array)};
-	            $ng->process_text(join(' ',@stemmed_words));
+	            		my @stemmed_words = @{$stemmer->stem(@text_array)};
+	            		$ng->process_text(join(' ',@stemmed_words));
 			}
 			
 			else {
@@ -74,8 +78,69 @@ while ((my $result = $search->next) && ($count < 50)) {
 }
 my @ngram_array = $ng->get_ngrams(orderby=>frequency);
 
-# AFFICHAGE DES RESULTATS:
-print $ng->to_string(orderby=>frequency);
+# AFFICHAGE DES RESULTATS
+my @lines = split('\n',$ng->to_string(orderby=>frequency));
+
+# EXPORT DES RESULTATS
+unless(open FILE, '>'.$export_file){
+	die "Impossible d'ouvrir le fichier $export_file";
+}
+print FILE $ng->to_string(orderby=>frequency);
+close FILE;
+
+if($csvfile != 0) {		
+	# creation des buffers CSV
+	my $csv1 = Class::CSV->new (fields => ["unigram","frequency"]);
+	$csv1->add_line(["unigram","frequency"]);
+
+	my $csv2 = Class::CSV->new (fields => ["bigram","frequency"]);
+	$csv2->add_line(["bigram","frequency"]);
+
+	my $csv3 = Class::CSV->new (fields => ["trigram","frequency"]);
+	$csv3->add_line(["trigram","frequency"]);
+
+	my $indicator = 0;
+	# remplissage des buffers csv
+	foreach my $line(@lines){
+		if ($line =~ /([A-Z]+|-)/){
+			$indicator++;
+		}
+		if ($indicator > 4){
+			if ($indicator < 6){
+				$csv1->add_line([split('\t',$line)]);
+			}
+		}
+		if ($indicator > 8){
+			if ($indicator < 10){
+				$csv2->add_line([split('\t',$line)]);
+			}
+		}
+		if ($indicator > 10){
+			if ($indicator < 14){
+				$csv3->add_line([split('\t',$line)]);
+			}
+		}
+		
+	}
+	
+	# creation des fichiers CSV
+
+	open my $fh1, ">:encoding(utf8)", "ckl_unigram.csv" or die "ckl_unigram.csv: $!";
+	open my $fh2, ">:encoding(utf8)", "ckl_bigram.csv" or die "ckl_bigram.csv: $!";
+	open my $fh3, ">:encoding(utf8)", "ckl_trigram.csv" or die "ckl_trigram.csv: $!";
+
+	my $csv_string1 = $csv1->string();
+	my $csv_string2 = $csv2->string();
+	my $csv_string3 = $csv3->string();
+
+	print $fh1 $csv_string1;
+	print $fh2 $csv_string2;
+	print $fh3 $csv_string3;
+
+	close $fh1;
+	close $fh2;
+	close $fh3;
+}
 
 =head1 NAME
 projet_ckl.pl - Analyse des 50 reponses de Google
@@ -91,4 +156,5 @@ Ce programme envoie une requete a Google,
 recupere les 50 premieres reponses, extrait le texte des pages,
 determine la langue (optionnel), lemmatise tous les mots (optionnel)
 et effectue une analyse en bigrammes et trigrammes.
+Si demande, il exporte les n-gram dans des fichiers csv separes.
 =cut
